4efe962527b44c5a99b9fb7cb3acb5eb3e6970cd
urutear <keitadance0208@gmail.com>
2024-12-17 21:42:20 +0900

    Add entry, not_found page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("entries/<str:title>", views.entry, name="entry"),
+    path("add", views.add_page, name="add"),
+    path("not_found", views.not_found, name="not_found"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
@@ -8,3 +10,28 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    print(f"entry: {type(entry)}")
+    if not entry:
+        return HttpResponseRedirect(reverse("not_found"))
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": entry
+    })
+    
+
+def add_page(request):
+    if request.method == "POST":
+        ...
+    else:
+        return render(request, "encyclopedia/add.html")
+
+def search_page(request):
+    ...
+
+def not_found(request):
+    return render(request, "encyclopedia/not_found.html", {
+        "error_code": 404,
+        "error": "Page not found"
+    })
